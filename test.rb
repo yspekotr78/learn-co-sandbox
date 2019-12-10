4fa9e44f0db17bf3d5323fa0c66111d00779d21a
Yury Spektor <yspektor78@gmail.com>
2019-12-10 19:39:31 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,11 @@
+def say_hello_world_five_times
+  puts "Hello World!"
+  puts "Hello World!"
+  puts "Hello World!"
+  puts "Hello World!"
+  puts "Hello World!"
+end
+ 
+say_hello_world_five_times
+puts "other work"
+say_hello_world_five_times
